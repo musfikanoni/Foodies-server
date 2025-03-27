ffe6c34369df2dbb2f4b2ab164086e521a595fcd
musfikanoni <musfikanoni@gmail.com>
2025-03-28 01:37:09 +0600

    created food apis

--- index.js
+++ index.js
@@ -27,11 +27,20 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const FoodsCollection = client.db('FoodiesDB').collection('foods');
+    
+    app.post('/foods', async (req, res) =>{
+      const newFood = req.body;
+      const result = await FoodsCollection.insertOne(newFood);
+      res.send(result);
+    })
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
