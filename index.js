9d84fccce543bda0f53b2a98bd9a0dc4429c211a
musfikanoni <musfikanoni@gmail.com>
2025-03-28 11:25:59 +0600

    get apis for sort

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -45,12 +45,19 @@ async function run() {
       const cursor = FoodsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
+    });
+
+    app.get('/foods/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await FoodsCollection.findOne(query);
+      res.send(result);
     })
 
-    // app.get('/', async (req, res) => {
-    //   const result = await servicesCollection.find().limit(8).toArray();
-    //   res.send(result);
-    // });
+    app.get('/', async (req, res) => {
+      const result = await FoodsCollection.find().limit(8).toArray();
+      res.send(result);
+    });
 
 
 
